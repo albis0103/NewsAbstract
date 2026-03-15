e9eeddd4c7d320400f06af8bf29c970ffd1de947
Albis <jasonhappywu@gmail.com>
2026-03-15 10:34:26 +0800

    ADD and JUSTIFY the comment let more clearify

--- keyvector_model.py
+++ keyvector_model.py
@@ -1,5 +1,6 @@
 from gensim.models import KeyedVectors
+#將模型預載入，讓news summary()以mmap讀取，加速
 print("loading the original .vec file (will wait about 10 sec~)")
 model = KeyedVectors.load_word2vec_format("models/cc.en.300.vec", binary=False, limit=100000)
-print("transform to .bin that can support mmap")
+print("transform to .kv and .npy that can support mmap")
 model.save("models/fast_model.kv")#會自動產生gensim專屬.kv and .npy支援mmap
