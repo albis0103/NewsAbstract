cee0a1fe361b6c0ac0f130bd5d91643cc8173211
Albis <jasonhappywu@gmail.com>
2026-05-13 14:04:34 +0800

    feat:<RSS insertion> new RSS web-crawler and UIi

---
+++ dashboard-api/main.py
@@ -0,0 +1,101 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel, HttpUrl
+from kafka import KafkaConsumer
+import os
+import threading
+import json
+from typing import List
+import time
+
+
+app = FastAPI(title='Dashboard API')
+#set CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # 開發階段先允許所有來源
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# setting mongodb atlas connection setting
+MONGO_URI = "mongodb+srv://jasonwu:jason0103@cluster0.xvreqja.mongodb.net/newsdb?authSource=admin&retryWrites=true&w=majority"
+client = AsyncIOMotorClient(MONGO_URI)
+db = client['newsdb']
+rss_collection = db['rss_source']
+
+#data model (pydantic)
+class RssSource(BaseModel):
+    url:str
+    name:str = "Custom RSS"
+    is_active: bool = True
+class RssSourceResponse(BaseModel):
+    url:str
+
+
+
+latest_news = []
+def consume_kafka():
+    # from backgrond, get the news from kafka persistently
+    kafkabroker = os.getenv("KAFKA_BROKER", "localhost:9092")
+    while True:
+        try:
+            consumer = KafkaConsumer(
+                'raw-security-news', #set Topic from crawler
+                bootstrap_servers=[kafkabroker], 
+                auto_offset_reset='earliest',
+                value_deserializer=lambda x : json.loads(x.decode('utf-8'))
+            )
+            
+            #push latest news to queue
+            for message in consumer:
+                latest_news.insert(0, message.value)
+                # avoid buffer overflow
+                if len(latest_news)>50:
+                    latest_news.pop()
+        except Exception as e:
+            print(f"Kafka connect ERROR!:{e}")
+            time.sleep(5)
+    
+#when fastapi start new thread to run consume_kafka 
+@app.on_event("startup")
+def startup_event():
+    thread = threading.Thread(target=consume_kafka,daemon=True )
+    thread.start()
+
+@app.get("/")
+def root():
+    return {"message":"Secops Dashboard api is running"}
+
+@app.get("/api/news")
+def get_latest_news():
+    return{
+        "status":"success", 
+        "count":len(latest_news),
+        "data":latest_news
+    }
+
+@app.get("/api/rss-sources", response_model=List[RssSourceResponse])
+async def get_rss_resource():
+    sources = []
+    cursor = rss_collection.find({"is_active":True})
+    async for doc in cursor:
+        sources.append(RssSourceResponse(url=doc["url"]))
+    return sources
+
+@app.post("/api/rss-sources")
+async def add_rss_source(source:RssSource):
+    existing = await rss_collection.find_one({"url":source.url})
+    if existing:
+        return {"status":"error", "message":"RSS already existed!"}
+    await rss_collection.insert_one(source.dict())
+    return {"status":"success", "message":"insert RSS source successful"}
+
+@app.delete("/api/rss-sources")
+async def delete_rss_resource(source:RssSource):
+    res = await rss_collection.delete_one({"url":source.url})
+    if res.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="source not found")
+    return {"status":"success", "message":"Successfully deleted"}
---
+++ news-crawler/crawler.py
@@ -0,0 +1,77 @@
+import feedparser
+from kafka import KafkaProducer
+import json
+import requests
+import time
+import os
+
+
+broker = os.getenv("KAFKA_BROKER", "localhost:9092")
+producer = None
+while producer is None:
+    try:
+        producer = KafkaProducer(
+            bootstrap_servers = [broker],
+            api_version=(0, 10),  
+            value_serializer = lambda v: json.dumps(v).encode('utf-8'),
+            request_timeout_ms=10000
+        )
+    except Exception as e:
+        print(f"Kafka not ready (NoBrokersAvailable), retrying in 5s... Error: {e}")
+def FetchRSS(rss_url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+        'Accept': 'application/rss+xml, application/xml, text/xml, */*'
+    }
+    try:
+        response = requests.get(rss_url, headers=headers, timeout=10)
+        if response.status_code == 200:
+            feed = feedparser.parse(response.content)
+            if feed.bozo:
+                print(feed.bozo_exception)
+
+            #get the entity need field
+            for entry in feed.entries:
+                news_data = {
+                    "title":entry.get("title", ""), 
+                    "link":entry.get("link", ""), 
+                    "summary":entry.get("summary", ""), 
+                    "publish":entry.get("publish", ""), 
+                    "categoies":[tag.term for tag in entry.get("tags", [])]
+                }
+
+                producer.send("raw-security-news", news_data)
+                print(f"send to kafka:{news_data['title']}")
+            producer.flush()
+        else:
+            print(f"web deny access:{response.status_code}")
+    except Exception as e:
+        print(f"passing{rss_url} error {e}")
+
+
+
+
+def run_crawler_job():
+    api_url = os.getenv("API_URL", "http://localhost:8001/api/rss-sources")
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            rss_list = response.json()
+            if not rss_list:
+                print("empty for RSS source")
+                return
+            print(f"get the {len(rss_list)} RSS sources")
+            for item in rss_list:
+                url = item['url']
+                print(f"start process source:{url}")
+                FetchRSS(url)
+            print("all RSS source crawler finish")
+        else:
+            print(f"can't get RSS list, api response:{response.status.code}")
+    except Exception as e:
+        print(f"API connection error{e}")
+
+if __name__ == "__main__":
+    while True:
+        run_crawler_job()
+        time.sleep(600)
